66379ddce270a4d172b5a29ac6f7dd8ebbb72032
Alexey Zimin <lexx.jock@gmail.com>
2022-07-17 18:13:58 +1000

    add branch junit4-platform

--- src/test/java/ru/netology/test/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/test/CashBackHackServiceTest.java
@@ -1,8 +1,12 @@
 package ru.netology.test;
 
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import ru.netology.servise.CashbackHackService;
+
 
 public class CashBackHackServiceTest {
 
@@ -18,6 +22,7 @@ public class CashBackHackServiceTest {
     }
 
     @Test
+    @Ignore
     public void testAmount1000() {
 
         CashbackHackService service = new CashbackHackService();
@@ -59,5 +64,61 @@ public class CashBackHackServiceTest {
         int expected = 1000;
 
         assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJUnitPlatformAmount900() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    @Disabled
+    public void testJUnitPlatformAmount1000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJUnitPlatformAmount1001() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJUnitPlatformAmount999() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJUnitPlatformAmount0() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        Assertions.assertEquals(expected, actual);
     }
 }
